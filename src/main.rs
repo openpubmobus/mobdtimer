1cb2466d808d9bcdd6dbbef6211ca50ce7a059b1
Yves Dorfsman <yves@zioup.com>
2021-07-05 21:16:58 -0600

    capturing argument

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use anyhow::Result;
+use std::env;
+//use async_std::task;
+//use chrono::Utc;
+use firebase_rs::*;
+//use notify_rust::Notification;
+//use std::sync::atomic::AtomicBool;
+//use std::sync::Arc;
+//use std::time::Duration;
+//use serde_json::{json, Value};
+
+static FIREBASE_URL: &str = "https://rust-timer-default-rtdb.firebaseio.com";
+static PROGNAME: &str = "mobdtimer";
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Need one argument, amount of time");
+        std::process::exit(1)
+    }
+    let duration = args[1].parse::<i32>();
+    if duration.is_err() {
+        eprintln!("argument needs to be numerical.");
+        std::process::exit(1)
+    }
+    println!("{:?}", duration);
+
+    let db: Firebase;
+    match firebase() {
+        Ok(f) => db = f,
+        Err(e) => {
+            eprintln!("Firebase connection error:");
+            eprintln!("{}", e);
+            std::process::exit(1)
+        }
+    }
+}
+
+fn firebase() -> Result<Firebase> {
+    Firebase::new(FIREBASE_URL).map_err(|e| e.into())
 }
