daac2fbb468f0c1b72aa19c4fa527a5bcf8b7cc7
Yves Dorfsman <yves@zioup.com>
2021-11-01 21:06:26 -0600

    added struct, commented future unused code

--- src/main.rs
+++ src/main.rs
@@ -18,24 +18,47 @@ mod string_util;
 static FIREBASE_URL: &str = "https://rust-timer-default-rtdb.firebaseio.com";
 static PROMPT: &str = "mobdtimer> ";
 
+struct TimerControl {
+   mutex: Mutex<bool>,
+   condvar: Condvar
+}
+
+struct Db {
+
+}
+
+/*
+ * Create two struct, one for firebase (connection + uid), and one for the Arc and friends
+ */
+
 fn main() {
-    let timer_control = Arc::new((Mutex::new(false), Condvar::new()));
+    // let timer_control_old = Arc::new((Mutex::new(false), Condvar::new()));
+
+    let timer_control = Arc::new(TimerControl {
+        mutex: Mutex::new(false),
+        condvar: Condvar::new()
+    });
 
     match process_args() {
         Ok(result) => {
             // println!("specified timer for {:?}", result);
 
+            let firebase_conn = firebase().unwrap();
+            let firebase_conn_clone = firebase_conn.clone();
+
             let repo_url = git::normalize_remote(&git::git_repo_url().unwrap());
             let repo_url_clone = repo_url.clone();
+
             println!("repo key: {}", repo_url);
 
             let timer_control_clone = Arc::clone(&timer_control);
-            thread::spawn(move || run_event_thread(repo_url_clone, &timer_control));
+            thread::spawn(move || run_event_thread(repo_url_clone, &timer_control, &firebase_conn));
             run_command_thread(
                 &repo_url,
                 &timer_control_clone,
                 io::stdin().lock(),
                 io::stdout(),
+                &firebase_conn_clone
             )
         }
         Err(message) => {
@@ -65,9 +88,10 @@ enum CommandResult {
 
 fn run_command_thread<R, W>(
     repo_url: &str,
-    timer_control: &Arc<(Mutex<bool>, Condvar)>,
+    timer_control: &Arc<TimerControl>,
     mut reader: R,
     mut writer: W,
+    firebase_conn: &Firebase
 ) where
     R: BufRead,
     W: Write,
@@ -77,7 +101,7 @@ fn run_command_thread<R, W>(
         io::stdout().flush().unwrap();
         let mut input = String::new();
         match reader.read_line(&mut input) {
-            Ok(_) => match handle_command(repo_url, timer_control, &input.trim()) {
+            Ok(_) => match handle_command(repo_url, timer_control, &input.trim(), firebase_conn) {
                 Ok(CommandResult::Exit) => return,
                 Ok(_) => continue,
                 Err(error) => writeln!(&mut writer, "{}", error).expect("Unable to write"),
@@ -91,8 +115,9 @@ fn run_command_thread<R, W>(
 
 fn handle_command(
     repo_url: &str,
-    timer_control: &Arc<(Mutex<bool>, Condvar)>,
+    timer_control: &Arc<TimerControl>,
     command: &&str,
+    firebase_conn: &Firebase
 ) -> Result<CommandResult, String> {
     match &command.split(' ').collect::<Vec<&str>>()[..] {
         [command] => match *command {
@@ -103,7 +128,7 @@ fn handle_command(
         },
         [command, arg] => match *command {
             "s" => {
-                create_timer(arg.to_string(), repo_url);
+                create_timer(arg.to_string(), repo_url, firebase_conn);
                 Ok(CommandResult::Continue)
             }
             _ => Err("invalid command".to_string()),
@@ -112,7 +137,7 @@ fn handle_command(
     }
 }
 
-fn create_timer(duration_in_minutes: String, repo_url: &str) {
+fn create_timer(duration_in_minutes: String, repo_url: &str, firebase_conn: &Firebase) {
     // TODO: blow up if not numeric
     let duration = duration_in_minutes.parse::<u64>().unwrap();
     println!(
@@ -120,14 +145,12 @@ fn create_timer(duration_in_minutes: String, repo_url: &str) {
         duration, repo_url
     );
 
-    let firebase_conn = firebase().unwrap();
-
     let end_time = store_future_time(&firebase_conn, None, duration, repo_url);
     println!("Timer started, id: {} end_time: {:?}", &repo_url, end_time);
 }
 
 fn store_future_time(
-    firebase: &Firebase,
+    firebase_conn: &Firebase,
     given_time: Option<i64>,
     wait_minutes: u64,
     uid: &str,
@@ -137,24 +160,27 @@ fn store_future_time(
         None => Utc::now().timestamp(),
     };
 
-    let end_time = start_time_epoch + (wait_minutes as i64) * 60;
-    let timer = firebase.at(uid)?;
-    timer.set(&format!("{{\"endTime\":{}}}", end_time))?;
+    let end_time_epoch = start_time_epoch + (wait_minutes as i64) * 60;
+    store_end_time(firebase_conn, uid, &end_time_epoch);
+    Ok(end_time_epoch)
+}
 
-    Ok(end_time)
+fn store_end_time(firebase_conn: &Firebase, uid: &str, end_time_epoch: &i64) {
+    let timer = firebase_conn.at(uid).unwrap();
+    timer.set(&format!("{{\"endTime\":{}}}", end_time_epoch));
 }
 
 fn firebase() -> Result<Firebase> {
     Firebase::new(FIREBASE_URL).map_err(|e| e.into())
 }
 
-fn run_event_thread(repo_url: String, timer_control: &Arc<(Mutex<bool>, Condvar)>) {
+fn run_event_thread(repo_url: String, timer_control: &Arc<TimerControl>, firebase_conn: &Firebase) {
     let url = format!("{}/{}.json", FIREBASE_URL, repo_url);
     let client = Client::new(Url::parse(&url).unwrap());
     for event in client {
         match event {
             Ok(good_event) => {
-                handle_event(good_event, timer_control);
+                handle_event(good_event, timer_control, firebase_conn);
                 //print!("{}", PROMPT);
                 io::stdout().flush().unwrap();
             }
@@ -163,50 +189,63 @@ fn run_event_thread(repo_url: String, timer_control: &Arc<(Mutex<bool>, Condvar)
     }
 }
 
-fn handle_event(event: Event, timer_control: &Arc<(Mutex<bool>, Condvar)>) {
+fn handle_event(event: Event, timer_control: &Arc<TimerControl>, firebase_conn: &Firebase) {
     if let Some(event_type) = event.event_type {
         if event_type.as_str() == "put" {
-            handle_put(event.data, timer_control)
+            handle_put(event.data, timer_control, firebase_conn)
         }
     }
 }
 
-fn handle_put(json_payload: String, timer_control: &Arc<(Mutex<bool>, Condvar)>) {
+fn handle_put(json_payload: String, timer_control: &Arc<TimerControl>, firebase_conn: &Firebase) {
     let node: Value = serde_json::from_str(&json_payload).unwrap();
     if let Some(end_time) = node["data"]["endTime"].as_i64() {
         if end_time > Utc::now().timestamp() {
-            start_timer(end_time, timer_control);
+            start_timer(end_time, timer_control, firebase_conn);
         }
     }
 }
 
-fn kill_timer(timer_control: &Arc<(Mutex<bool>, Condvar)>) -> CommandResult {
-    let (lock, cvar) = &**timer_control;
-    let mut started = lock.lock().unwrap();
-    *started = true;
-    cvar.notify_one();
+fn kill_timer(timer_control: &Arc<TimerControl>) -> CommandResult {
+    //let (lock, cvar) = &**timer_control;
+    let TimerControl {mutex, condvar} = &**timer_control;
+    let mut kill_timer_flag = mutex.lock().unwrap();
+    *kill_timer_flag = true;
+    condvar.notify_one();
     CommandResult::Continue
 }
 
-fn start_timer(end_time: i64, timer_control: &Arc<(Mutex<bool>, Condvar)>) {
-    // wait for the thread to start up
-    let (lock, cvar) = &**timer_control;
-    let mut started = lock.lock().unwrap();
-    // as long as the value inside the `Mutex<bool>` is `false`, we wait
-    let duration = (end_time - Utc::now().timestamp()).unsigned_abs();
+/*
+struct TimerControl {
+   mutex: Mutex<bool>,
+   condvar: Condvar
+}
+ */
+
+fn start_timer(end_time: i64, timer_control: &Arc<TimerControl>, _firebase_conn: &Firebase) {
+    // let (lock, cvar) = &**timer_control;
+    //let timer_control_struct = &**timer_control;
+    // let TimerControl { lock: mutex, cvar: condvar } = timer_control_struct;
+    let TimerControl {mutex, condvar} = &**timer_control;
+    let mut kill_timer_flag = mutex.lock().unwrap();
+
+    let duration_in_seconds = (end_time - Utc::now().timestamp()).unsigned_abs();
+
     loop {
-        let result = cvar
-            .wait_timeout(started, Duration::from_secs(duration))
+        let result = condvar
+            .wait_timeout(kill_timer_flag, Duration::from_secs(duration_in_seconds))
             .unwrap();
-        started = result.0;
-        if *started {
-            // We received the notification and the value has been updated, we can leave.
-            println!("Got Interrupted");
+        kill_timer_flag = result.0;
+        if *kill_timer_flag {
+            println!("timer killed");
+            //store_end_time(firebase_conn, uid, &end_time_epoch);
             break;
         } else if result.1.timed_out() {
-            println!("WE'RE DONE");
+            println!("timer completed");
+            //store_end_time(firebase_conn, uid, &end_time_epoch);
             break;
         }
+        // else: spurious wakeup; restart loop
     }
 }
 
